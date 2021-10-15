2a6cb2d578ca907fd77c94812dc451ff5b48ba3f
laxman939 <ramlaxman947@gmail.com>
2021-10-15 08:57:32 +0530

    forIn and forOf

--- ForIn-ForOf/index.js
+++ ForIn-ForOf/index.js
@@ -1,10 +1,12 @@
 //for in and for applicable for arrays only
 const obj = ["ram", "kiran", "age"];
 
+//for of displays elements
 for (let e of obj) {
   console.log(e); //ram kiran age
 }
 
+//for in displays index values
 for (let e in obj) {
   console.log(e); //0 1 2
 }
