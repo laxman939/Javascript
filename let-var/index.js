61b51596c4999c1e14bcbd552af076ef0fae9d41
laxman939 <ramlaxman947@gmail.com>
2021-10-15 10:16:13 +0530

    let and var

---
+++ let-var/index.js
@@ -0,0 +1,56 @@
+/*let vs var
+let-->1.Block scoped, 2.can't redeclare, 3.Hoisting not occur
+var-->1.Function scoped, 2.can redeclare, 3.Hoisting occurs*/
+//Hoisting is JavaScript's default behavior of moving declarations to the top.
+
+//1-Scope
+function sum1(n) {
+  let value = 0;
+  for (let i = 1; i <= n; i++) {
+    //console.log(i); //Accessed
+    value += i;
+  }
+  //console.log(i); //undefined-->Block scoped(Only in if block)
+  return value;
+}
+//console.log(sum1(5));
+
+function sum2(n) {
+  var value = 0;
+  for (var i = 1; i <= n; i++) {
+    value += i; //var anywhere inside block as well as function
+  }
+  console.log(i); //4-->accessed
+  return value;
+}
+sum2(3);
+
+//2-Redeclare
+var a = 6;
+var a = 4;
+//console.log(a); //4
+
+let b = 5;
+//let b = 2; //b has already declared
+//console.log(b)
+
+let d = 2;
+for (let d = 0; a < 3; a++) {
+  console.log("hello");
+}
+console.log(d); // 2-->for loop doesn't change outer variable
+
+var k = 2;
+for (var k = 0; a < 3; a++) {
+  console.log("hello");
+}
+console.log(k); // 0 -->for loop doesn't change outer variable
+
+//3-Hoisting
+console.log(x);
+var x = 5; //undefined(not an error)
+
+console.log(y); //Cannot access 'y' before initialization
+let y = 4;
+
+//Note-->Both let and var can be used as global variables
