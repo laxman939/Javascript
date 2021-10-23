a7f3c103ce4bc3a5e8454152f9b6783022d42bf4
laxman939 <ramlaxman947@gmail.com>
2021-10-23 17:50:33 +0530

    counting vowels

--- Regular-Expression/examples.js
+++ Regular-Expression/examples.js
@@ -2,9 +2,16 @@
 function upperCase(name) {
   reg = /^[A-Z]/; //^-->for starting
   if (reg.test(name)) {
-    console.log("First character is uppercase");
+    console.log("First character is in uppercase");
   } else {
-    console.log("First character is not uppercase");
+    console.log("First character is not in uppercase");
   }
 }
 upperCase("laxman"); //First character is not uppercase
+
+//2--Count No of vowels in a string
+function vowelsCount(str) {
+  return str.match(/[aeiou]/gi).length;
+}
+
+console.log(vowelsCount("Hi everyone")); //5
