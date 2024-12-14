d22dfd8cb30a2e31b9be72bf39f45e56108c0779
laxman <ramlaxman947@gmail.com>
2024-12-14 10:12:39 +0530

    web workers with real time examples

--- web-workers/main.js
+++ web-workers/main.js
@@ -1,8 +1,11 @@
 // Create a new Web Worker using Worker()
 const worker = new Worker("worker.js");
 
+const data = {
+  number: 40,
+};
 // Send data to the worker.
-worker.postMessage(4);
+worker.postMessage(data);
 
 // Receive data from the worker
 worker.onmessage = function (e) {
@@ -13,3 +16,27 @@ worker.onmessage = function (e) {
 worker.onerror = function (err) {
   console.log("worker error: ", err);
 };
+// Practical examples
+//Example 1: Sorting a Massive Array
+const largeArray = Array.from({ length: 1000 }, () =>
+  Math.floor(Math.random() * 1000000)
+);
+data.largeArray = largeArray;
+worker.postMessage(data);
+
+worker.onmessage = function (e) {
+  console.log("Sorted arrray: ", e.data, e);
+};
+
+// Example 2: Fetching Data from an API
+
+worker.postMessage({ url: "https://fakestoreapi.com/products" });
+worker.onmessage = function (e) {
+  if (e.data.error) {
+    console.log("API Error: ", e.data.error);
+  } else {
+    console.log("Fetched data: ", e.data);
+  }
+};
+
+// Why Web workers executing before main thread?
--- web-workers/worker.js
+++ web-workers/worker.js
@@ -7,18 +7,46 @@
 
 // Web workers communicate with the main thread through messaging pipeline to send or receive messages
 
+// Main Thread: I need to stay focused on keeping the UI responsive
+// Web Worker: Got it! I will handle the tough stuff like data fetching
+
 // postMessage -- send data to the worker
 // onMessage -- receive processed data from the worker
 // onError -- handles errors
 
 self.onmessage = function (e) {
-  const number = e.data;
+  const number = e.data.number;
   const result = fibonacci(number);
   postMessage(result);
 };
-j;
 
 function fibonacci(n) {
   if (n <= 1) return n;
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
+
+// Example 1: Sorting a Massive Array
+// Sorting a huge array can take a lot of time. Let’s let a Web Worker handle that.
+self.onmessage = function (e) {
+  const array = e.data.largeArray;
+  array.sort((a, b) => a - b);
+  postMessage(array);
+};
+
+// Example 2: Fetching Data from an API
+self.onmessage = async function (e) {
+  console.log(e, "appi worker");
+
+  const url = e.data.url;
+  console.log(url);
+
+  try {
+    const response = await fetch(url);
+    console.log(response, "resp");
+
+    const data = await response.json();
+    postMessage(data);
+  } catch (err) {
+    postMessage({ error: "Failed to fetch data from API" });
+  }
+};
